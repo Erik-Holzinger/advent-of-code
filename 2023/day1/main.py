acb416b434b2ee427e116fb87b208f16b00c2128
Erik Holzinger <e.holzinger@bd-holzinger.de>
2023-12-02 14:17:42 +0100

    Implement exercise b

--- 2023/day1/main.py
+++ 2023/day1/main.py
@@ -1,7 +1,40 @@
 import re
 
+
+def exercise_a():
+    with open("input.txt") as file:
+        print(calc_sum(file.readlines()))
+
+
+def exercise_b():
+    with open("input.txt") as file:
+        lines = []
+        for line in file:
+            new_line = line.strip()
+            for k, v in number_map.items():
+                new_line = new_line.replace(k, v)
+            lines.append(new_line)
+        print(calc_sum(lines))
+
+
+number_map = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
+
+
+def calc_sum(lines):
+    return sum([int(re.sub("[^0-9]", "", line)[0]) * 10 +
+                int(re.sub("[^0-9]", "", line)[-1]) for line in lines])
+
+
 if __name__ == '__main__':
-    with open("input_1.txt") as file:
-        lines = [line.strip() for line in file]
-        sum = [line.isdigit() for line in lines]
-        print(sum)
+    exercise_a()
+    exercise_b()

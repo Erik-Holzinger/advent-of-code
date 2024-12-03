080525fc0226f931a5bcf2a7f0cd96d9f9fdace7
Erik Holzinger <e.holzinger@bd-holzinger.de>
2024-12-03 11:10:03 +0100

    feat(day1): solve day1

---
+++ 2024/day1/day1.py
@@ -0,0 +1,34 @@
+import re
+from pathlib import Path
+from typing import Tuple, List
+
+
+def get_input(input_file: Path) -> Tuple[List[int], List[int]]:
+    left, right = [], []
+
+    with open(input_file) as f:
+        for line in f:
+            x, y = re.findall(r'\d+', line)
+            left.append(int(x))
+            right.append(int(y))
+
+    return left, right
+
+
+def solve_1a(input_file: Path):
+    left, right = get_input(input_file)
+
+    result = 0
+    for (x, y) in zip(sorted(left), sorted(right)):
+        result += abs(x - y)
+    print("Result 1a: " + str(result))
+
+
+def solve_1b(input_file: Path):
+    left, right = get_input(input_file)
+
+    result = 0
+    for x in left:
+        y = right.count(x)
+        result += x * y
+    print("Result 1b: " + str(result))
---
+++ 2024/main.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+from day1.day1 import solve_1a, solve_1b
+
+if __name__ == '__main__':
+    solve_1a(Path("./day1/input.txt"))
+    solve_1b(Path("./day1/input.txt"))

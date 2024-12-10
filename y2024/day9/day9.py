f3796a4ba10b4d33a7aae6b8c57ff006bf0fb19d
Erik Holzinger <e.holzinger@bd-holzinger.de>
2024-12-10 10:08:17 +0100

    feat(day9): finally solved 9b

--- y2024/day9/day9.py
+++ y2024/day9/day9.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from pathlib import Path
 from typing import List
 
@@ -44,76 +45,68 @@ def sort_quicker(puzzle_input: List[str]) -> List[str]:
     return puzzle_input
 
 
-def sort_quicker_blocks(puzzle_input: List[str]) -> List[str]:
+def sort_quicker_with_blocks(puzzle_input: List[str]) -> List[str]:
+    last_index = len(puzzle_input) - 1
     left = 0
-    right = len(puzzle_input) - 1
+    right = last_index
+    # Lookup map for block size
+    block_size = Counter([x for x in puzzle_input if x != "."])
 
-    while right >= 0:
-        # Search for non-dot
-        while right >= 0 and puzzle_input[right] == ".":
-            right -= 1
-
-        # Calculate needed space
-        space_right = 1
-        right_search = right - 1
-        while right_search > 0 and puzzle_input[right_search] == puzzle_input[right]:
-            space_right += 1
-            right_search -= 1
-
-        # Search for dot
-        while left < len(puzzle_input) and puzzle_input[left] != ".":
+    # Sort every element exactly once to the left side
+    while right > 0:
+        # Search for free space
+        while left < last_index and puzzle_input[left] != ".":
             left += 1
+        left_ahead = left + 1
+        found_space = 1
+        while left_ahead < last_index and puzzle_input[left_ahead] == ".":
+            left_ahead += 1
+            found_space += 1
 
-        # Calculate space on left side
-        space_left = 1
-        left_search = left + 1
-        while left_search < len(puzzle_input) and puzzle_input[left_search] == ".":
-            space_left += 1
-            left_search += 1
+        # Search for block to move
+        while right > 0 and puzzle_input[right] == ".":
+            right -= 1
+        needed_space = block_size[puzzle_input[right]]
 
-        if left_search > right:
+        # Would move the element further away so stop and reset
+        if right < left:
+            right -= needed_space
             left = 0
-            right = right_search
             continue
 
-        if space_left >= space_right:
-            # Block fits
-            for i in range(space_right):
+        if needed_space <= found_space:
+            # Found space, move block
+            for i in range(needed_space):
                 puzzle_input[left + i] = puzzle_input[right - i]
                 puzzle_input[right - i] = "."
-            left += space_right
-            right = right_search
+
+            right -= needed_space
+            left = 0
         else:
-            # Block does not fit
-            left = left_search
+            # Not enough space, search for next one
+            left = left_ahead
 
     return puzzle_input
+
+
+def __calculate_checksum(puzzle_input: List[str]) -> int:
+    checksum = 0
+    for i in range(len(puzzle_input)):
+        if puzzle_input[i] == ".":
+            break
+        checksum += i * int(puzzle_input[i])
+    return checksum
 
 
 @timer
 def solve_9a(input_file: Path) -> int:
     puzzle_input: List[str] = __load_puzzle_input(input_file)
     puzzle_input = sort_quicker(puzzle_input)
-
-    checksum = 0
-    for i in range(len(puzzle_input)):
-        if puzzle_input[i] == ".":
-            break
-        checksum += i * int(puzzle_input[i])
-
-    return checksum
+    return __calculate_checksum(puzzle_input)
 
 
 @timer
 def solve_9b(input_file: Path) -> int:
     puzzle_input: List[str] = __load_puzzle_input(input_file)
-    puzzle_input = sort_quicker_blocks(puzzle_input)
-    print(puzzle_input)
-
-    checksum = 0
-    for i in range(len(puzzle_input)):
-        if puzzle_input[i] == ".":
-            continue
-        checksum += i * int(puzzle_input[i])
-
-    return checksum
+    puzzle_input = sort_quicker_with_blocks(puzzle_input)
+    return __calculate_checksum(puzzle_input)
